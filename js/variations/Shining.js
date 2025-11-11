d648cc0aad1f34bb94e7427bbfd5c48f2cf412cf
pippinbarr <pippin.barr@gmail.com>
2025-11-11 15:54:43 -0500

    Not so shining
    - Getting pretty sick of thinking through the "best" way to implement this with data structures
    - It's a bit funny because I think the struggles coincide with teaching data-driven stuff this week in my class and thus wanting to come up with an elegant solution where actually a shitty solution would be totally fine
    - The transition between "biomes" is the current sticking point, bit of a pain in the assssssss
    - Gonna move on to something else for a bit

--- js/variations/Shining.js
+++ js/variations/Shining.js
@@ -12,11 +12,36 @@ class Shining extends Racer {
 
         this.playerColour = 0xfce088;
         this.waterColour = 0x112f77;
-        this.grassGroundColour = 0x38450a;
+        this.grassColour = 0x38450a;
         this.greenTreeColour = 0x326c25;
         this.snowGroundColour = 0xc3c3c3;
         this.whiteTreeColour = 0xffffff;
 
+        this.scenes = [
+            {
+                name: "lakeside",
+                left: {
+                    tree: {
+                        alpha: 0,
+                        color: this.greenTreeColour
+                    },
+                    ground: {
+                        alpha: 1,
+                        color: this.waterColor
+                    }
+                },
+                right: {
+                    tree: {
+                        alpha: 1,
+                        color: this.greenTreeColour
+                    },
+                    ground: {
+                        alpha: 1,
+                        color: this.grassColour
+                    }
+                }
+            }
+        ];
     }
 
     create() {
@@ -33,15 +58,9 @@ class Shining extends Racer {
         this.player.x = this.width / 2 + this.laneWidth / 2;
         this.player.setTint(this.playerColour);
 
-        // Add lake
-        this.lake = this.physics.add.image(0, 0, 'road-mark')
-            .setScale(this.laneWidth * 2, this.height)
-            .setTint(this.waterColour)
+        this.createSceneMaterials();
 
-        this.rightGround = this.physics.add.image(this.width, this.height / 2, 'road-mark')
-            .setScale(this.laneWidth * 2, this.height)
-            .setTint(this.grassGroundColour)
-        this.createTrees();
+        this.setupScene();
     }
 
     createDividers() {
@@ -49,45 +68,59 @@ class Shining extends Racer {
         this.addRoadMarks(2, this.dividersGroup);
     }
 
-    createTrees() {
-        this.treesGroup = this.physics.add.group();
+    createSceneMaterials() {
 
-        const stepY = this.pixelScale * 13;
+        this.leftTreesGroup = this.createSceneGroup(this.laneWidth / 2, 'tree');
+        this.rightTreesGroup = this.createSceneGroup(3 * this.laneWidth, 'tree');
 
-        this.treesGroup.createMultiple({
-            key: 'tree',
-            repeat: (this.height + stepY) / stepY,
+        this.groups = [this.leftTreesGroup, this.rightTreesGroup];
+
+        this.wrapper = this.physics.add.image(this.width / 2, this.height, 'road-mark')
+            .setScale(this.width, this.pixelScale);
+
+        this.physics.add.overlap(this.leftTreesGroup, this.wrapper, this.wrap, null, this);
+        this.physics.add.overlap(this.rightTreesGroup, this.wrapper, this.wrap, null, this);
+    }
+
+
+    wrap(wrapper, tree) {
+        tree.y -= this.height;
+    }
+
+    createSceneGroup(x, key) {
+        const group = this.physics.add.group();
+        const height = this.pixelScale * 12;
+
+        group.createMultiple({
+            key: key,
+            repeat: this.height / height,
             setXY: {
-                x: this.width - this.pixelScale * 1.5,
-                y: -stepY,
-                stepY: stepY
+                x: x,
+                y: 0,
+                stepY: height
             },
             setScale: {
                 x: this.pixelScale,
-                y: this.pixelScale
-            },
+                y: this.pixelScale,
+            }
         });
-        this.treesGroup.setTint(this.greenTreeColour);
-        // Make the markings move according to the player's speed
-        this.treesGroup.setVelocity(0, this.player.speed);
+
+        group.setVelocity(0, this.player.speed);
+        return group;
+    }
+
+    setupScene() {
+        this.currentScene = this.scenes[0];
+
+        this.leftTreesGroup.setTint(this.currentScene.left.treeColor);
+        this.leftTreesGroup.setAlpha(this.currentScene.left.treeAlpha);
     }
 
     update() {
         super.update();
-
-        this.wrapTrees();
     }
 
     moveDividers() {
 
-    }
-
-    wrapTrees() {
-        // Wrapping road divider
-        this.treesGroup.getChildren().forEach((tree) => {
-            if (tree.y - tree.displayHeight / 2 >= this.height) {
-                tree.y -= (this.height + tree.displayHeight * 1.75);
-            }
-        });
     }
 }
